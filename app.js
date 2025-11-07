b8c162a03692029f0ff5cf0b72c9f5d2b59370d6
sammie004 <123166502+sammie004@users.noreply.github.com>
2025-11-07 01:16:03 +0100

    new changes, the last , hopeflly

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.json())
 // app.use(bodyParser)
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // require
 const predict = require('./routes/intent-route')
